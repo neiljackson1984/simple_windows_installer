29099bbd450eeb4d7ef96ff3e3adafecdc8ce585
Neil Jackson <neiljackson1984@gmail.com>
2016-08-06 15:44:30 -0700

    modify saple install.bat to include a mention of how to use the 'sysnative' virtual folder to copy files into the windows system32 directory

--- deployment/install.bat
+++ deployment/install.bat
@@ -9,6 +9,9 @@ echo Now installing [[[[ PUT SOMETHING MEANINGFUL HERE ]]]]
 Powershell.exe -NonInteractive -command "Set-ExecutionPolicy Unrestricted"
 Powershell.exe -NonInteractive -command "%directoryOfThisScript%[[[[[[[[NAME OF POWERSHELL SCRIPT TO EXECUTE]]]]]].ps1"
 
+@REM if you need to copy files into c:\windows\system32, use the 'sysnative' virtual directory as in the below example.
+copy /Y "%directoryOfThisScript%SysinternalsSuite\*" "%SystemRoot%\Sysnative\"
+
 echo.
 echo This window will close in 20 seconds...
 timeout /t 20 >nul
